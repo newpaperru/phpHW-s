4a9724e4f06ecab955f4522c04089abe341c19dd
Boris Arkhangelskiy <boristar010205@icloud.com>
2024-09-25 19:35:56 +0300

    redone task 1

--- bin/mamp/homework1.php
+++ bin/mamp/homework1.php
@@ -1,7 +1,10 @@
 <?php
 
+
+
 // first task
 $arrayFirst = [1,1,2,3,4,5];
 $arraySecond = [1,6,3,8];
 $result = array_merge($arrayFirst, $arraySecond);
 echo 'Результат: ' . implode(', ', $result);
+
